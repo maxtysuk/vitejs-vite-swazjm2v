ff52287093e88092553b425a81ae658dcef9fda2
maxtysuk <151860054+maxtysuk@users.noreply.github.com>
2024-12-12 18:45:40 +0200

    Update Users.jsx

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -3,29 +3,22 @@ import { useEffect, useState } from 'react';
 
 export default function Users() {
     const [users, setUsers] = useState([]);
-    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('https://dummyjson.com/users')
             .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
+                if (response.ok) {
+                    return response.json();
                 }
-                return response.json();
+                return Promise.reject();
             })
             .then((data) => {
                 if (data.users) {
                     setUsers(data.users);
-                } else {
-                    throw new Error('Invalid API response format');
                 }
-            })
-            .catch((error) => {
-                setError(error.message);
             });
     }, []);
 
-    if (error) return <div>Error: {error}</div>;
     if (users.length === 0) return <div>Loading...</div>;
 
     return (
