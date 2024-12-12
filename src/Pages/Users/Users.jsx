323b6e076333222f1266a5a626f97947aefcf0dc
maxtysuk <151860054+maxtysuk@users.noreply.github.com>
2024-12-12 17:43:25 +0200

    Users.jsx

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,13 +1,38 @@
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Users(){
-    return (
-        <div className="Main">
-            <h1>USERS</h1>
-            <ul>
-                <li><Link to='/users/john'>John Doe</Link></li>
-                <li><Link to='/users/jane'>Jane Doe</Link></li>
-            </ul>
-        </div>
-    );
-}
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Завантажуємо дані за допомогою fetch
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        // Фільтруємо отримані дані
+        const filteredUsers = data.users.map((user) => ({
+          id: user.id,
+          name: `${user.firstName} ${user.lastName}`,
+          email: user.email,
+        }));
+        setUsers(filteredUsers);
+      })
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {/* Формуємо посилання для кожного користувача */}
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Users;
