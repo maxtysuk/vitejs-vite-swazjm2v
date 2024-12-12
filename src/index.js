0a47c790373773eec0aff172eac37495a26a8911
maxtysuk <151860054+maxtysuk@users.noreply.github.com>
2024-12-12 18:39:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter,
-createRoutesFromElements,
-RouterProvider, 
-Route, Outlet } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Outlet } from 'react-router-dom';
 
 import Header from './Pages/Header/Header';
 import Sidebar from './Pages/Sidebar/Sidebar';
@@ -14,40 +11,44 @@ import Users from './Pages/Users/Users';
 import UserPage from './Pages/Users/UserPage';
 import './global.css';
 import ErrorPage from './Pages/Error/Error';
-import usersData from './data/users.json';
 
-const Root = () =>{
-return(
-    <div className='container'>
+const Root = () => {
+    return (
+        <div className='container'>
             <Header />
             <Sidebar />
             <Outlet />
-    </div>
-)
+        </div>
+    );
+};
+
+async function loader({ params }) {
+    try {
+        const response = await fetch(`https://dummyjson.com/users/${params.userId}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch user data: ${response.status}`);
+        }
+        return response.json();
+    } catch (error) {
+        console.error(error);
+        throw new Response("User not found", { status: 404 });
+    }
 }
 
 const router = createBrowserRouter(
-   createRoutesFromElements(
-    <Route path='/' element={<Root />}>
-        <Route index element={<Welcome />} />
-        <Route path='community' element={<Community/>}/>
-        <Route path='about' element={<About/>}/>
-        <Route path='users' element={<Users/>}/>
-        <Route path='users/:userId' loader={loader} element={<UserPage/>} 
-        errorElement={<ErrorPage/>}/>
-        <Route path='*' element={<ErrorPage/>} />
-
-    </Route>
-   ) 
-)
+    createRoutesFromElements(
+        <Route path='/' element={<Root />}>
+            <Route index element={<Welcome />} />
+            <Route path='community' element={<Community />} />
+            <Route path='about' element={<About />} />
+            <Route path='users' element={<Users />} />
+            <Route path='users/:userId' loader={loader} element={<UserPage />} errorElement={<ErrorPage />} />
+            <Route path='*' element={<ErrorPage />} />
+        </Route>
+    )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <RouterProvider router={router} />
+    <RouterProvider router={router} />
 );
-
-function loader({params}){
-//console.log(params.userId);
-const user = usersData.filter(e => e.id === params.userId);
-return user[0];
-}
