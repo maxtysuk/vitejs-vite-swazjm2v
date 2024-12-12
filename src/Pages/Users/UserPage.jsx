88dfd6d41d89b856e7b84f89d72255f566b015d5
maxtysuk <151860054+maxtysuk@users.noreply.github.com>
2024-12-12 18:58:04 +0200

    Update UserPage.jsx

--- src/Pages/Users/UserPage.jsx
+++ src/Pages/Users/UserPage.jsx
@@ -1,25 +1,7 @@
 import { Link, useLoaderData } from 'react-router-dom';
 
-export async function loader({ params }) {
-    const userId = params.userId;
-    const response = await fetch(`https://dummyjson.com/users/${userId}`);
-    const user = await response.json();
-    return user;
-}
-
 export default function UserPage() {
     const user = useLoaderData();
-
-    if (!user) {
-        return (
-            <div className="Main user-page">
-                <div>
-                    <Link to="/users">Back</Link>
-                </div>
-                <h2>User not found</h2>
-            </div>
-        );
-    }
 
     return (
         <div className="Main user-page">
